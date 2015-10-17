d972b45a8ce9bca3c4d7f7ebb7d1f2f6c5f98c34
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-10-17 09:19:10 +0530

    Priority Scheduling Algorithm (Non-Preemptive)

--- Process-Scheduling-Algorithms/Priority.cpp
+++ Process-Scheduling-Algorithms/Priority.cpp
@@ -1,8 +1,16 @@
+/*
+ * Priority Scheduling Algorithm (Non-Preemptive)
+ *
+ * TCT -> Total Completion Time
+ * TAT -> Turn Around Time
+ * WT  -> Waiting Time
+*/
 #include <iostream>
 
 using std::cin;
 using std::cout;
 
+// This structure holds the basic data of a process
 typedef struct pri
 {
     int process;
@@ -11,7 +19,7 @@ typedef struct pri
     int priority;
     int tat;
     int wt;
-}pri;
+}pri;   //Data of type pri
 
 int sort (pri [], int);
 
@@ -22,7 +30,7 @@ int main()
     
     cout << "Enter number of processes: ";
     cin >> n;
-    pri arr[n];
+    pri arr[n];     //Array of type pri
     
     for (i = 0; i < n; i++)
     {
@@ -35,23 +43,33 @@ int main()
         cin >> arr[i].priority;
     }
     sort (arr, n);
-    arr[0].tat = TCT = arr[0].burst;
-    arr[0].wt = arr[0].tat - arr[0].burst;
+    arr[0].tat = TCT = arr[0].burst;        //For the first process the TAT is its TCT.
+    arr[0].wt = arr[0].tat - arr[0].burst;  //WT is calculated with TAT - Burst
+    // As the priority scheduling is non-preemptive, the first process when gets completed
+    // set the arrival time to -1, so that when sorted it will reach at the top of the list.
     arr[0].arrival = -1;
+    // Sort the structure completely
     sort (arr, n);
+    // Increment the process count
     count_process = 1;
     
+    // Repeat the process until the all the process have arrived and completed
     while(count_process < n)
     {
+        // Minimum priority will be set to 99
         minPri = 99;
         count = 0;
         i = count_process;
+        
+        // Check how many process have arrived in the given arrival time
         while (TCT >= arr[i].arrival && i < n)
         {
             count++;
             i++;
         }
         
+        // Check the priorities of the arrived processes and set the minimum priority to
+        // the minimum priority among the arrived processes
         for (j = i - count; count != 0 && j < n; j++, count--)
         {
             if (arr[j].priority < minPri)
@@ -60,6 +78,7 @@ int main()
                 pos = j;
             }
         }
+        // Calculate the TCT, TAT and WT of the processes and set their arrival to -1
         TCT = TCT + arr[pos].burst;
         arr[pos].tat = TCT - arr[pos].arrival;
         arr[pos].wt = arr[pos].tat - arr[pos].burst;
