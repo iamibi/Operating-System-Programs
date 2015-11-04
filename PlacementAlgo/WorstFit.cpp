aae7248aa8c65b03ed24acab1869dd9c9b317ff2
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-11-04 21:05:01 +0530

    Worst Fit Page Placement Algorithm

--- PlacementAlgo/WorstFit.cpp
+++ PlacementAlgo/WorstFit.cpp
@@ -1,65 +1,71 @@
+/*
+ * Worst Fit Page Placement Algorithm
+ *
+ * This program is dynamically created for the worst fit page placement algorithm
+ *
+ * Developed by Prakhar and Ibrahim
+*/
 #include <iostream>
 
 using std::cin;
 using std::cout;
 
-typedef struct placement
+typedef struct frame
 {
-    int no;
-    int blockSize;
-}placement;
+    int no;                     //Frame number
+    int blockSize;              //Frame Block Size
+}frame;
 
-typedef struct process
+typedef struct page
 {
-    int pro;
-    int no;
-    int blockno;
-}process;
+    int pro;                    //Page size
+    int no;                     //Page number
+    int blockno;                //Block Number the page is allocated to
+}page;
 
-int sort (placement [], int);
+int sort (frame [], int);
 
 int main()
 {
     int i, j, n, m;
     
-    cout << "Enter the total number of free blocks: ";
+    cout << "Enter the total number of free frames: ";
     cin >> n;
-    placement arr[n];
+    frame arr[n];
     
     for (i = 0; i < n; i++)
     {
         arr[i].no = i + 1;
-        cout << "Enter the" << arr[i].no << " block size: ";
+        cout << "Enter the " << arr[i].no << " block size: ";
         cin >> arr[i].blockSize;
     }
-    cout << "Free Block list\nBlock No\tBlock Size\n";
+    cout << "Free Frame list\nBlock No\tBlock Size\n";
     for (i = 0; i < n; i++)
         cout << arr[i].no << "\t\t" << arr[i].blockSize << "\n";
         
-    cout << "Enter the number of process: ";
+    cout << "Enter the number of pages: ";
     cin >> m;
-    process pr[m];
+    page pr[m];
     
     for (i = 0; i < m; i++)
     {
         pr[i].no = i + 1;
-        cout << "Enter the process " << pr[i].no << " size: ";
+        cout << "Enter the page " << pr[i].no << " size: ";
         cin >> pr[i].pro;
     }
-    cout << "Processes available\nProcess No\tProcess Size\n";
+    cout << "Page available\nPage No\tPage Size\n";
     for (i = 0; i < m; i++)
         cout << pr[i].no << "\t\t" << pr[i].pro << "\n";
 
     for (i = 0; i < m; i++)
-    {
         pr[i].blockno = -1;
-    }
     
     sort (arr, n);
     for (i = 0; i < m; i++)
     {
         for (j = 0; j < n; j++)
         {
+            //if the size of the page is less than the current block size
             if (pr[i].pro <= arr[j].blockSize)
             {
                 arr[j].blockSize -= pr[i].pro;
@@ -69,18 +75,16 @@ int main()
             }
         }
     }
-    cout << "Process No\tBlock No\n";
+    cout << "Page No\tBlock No\n";
     for (i = 0; i < m; i++)
-    {
         cout << "\t" << pr[i].no << "-->\t " << pr[i].blockno << "\n";
-    }
     return 0;
 }
 
-int sort (placement arr[], int n)
+int sort (frame arr[], int n)
 {
     int i, j;
-    placement temp;
+    frame temp;
     
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
