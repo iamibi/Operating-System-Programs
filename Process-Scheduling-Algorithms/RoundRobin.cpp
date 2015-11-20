71e3cecda979051a45a646b830b11929f2e2b174
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-11-20 10:36:10 +0530

    Round Robin Process Scheduling Algorithm

--- Process-Scheduling-Algorithms/RoundRobin.cpp
+++ Process-Scheduling-Algorithms/RoundRobin.cpp
@@ -1,11 +1,11 @@
 /*
-
-
-Program is incomplete.
-
-
-
+ *
+ * ROUND ROBIN PROCESS SCHEDULING ALGORITHM
+ *
+ * Developed by Ibrahim
+ *
 */
+
 #include <iostream>
 
 using std::cin;
@@ -13,24 +13,28 @@ using std::cout;
 
 typedef struct RR
 {
-    int process;
-    int burst;
-    int arrival;
-    int remaining;
+    int process;        //Process number
+    int burst;          //Burst time
+    int arrival;        //Arrival time
+    int remaining;      //Remaining time
+    int complete;       //Completion time
+    int tat;            //Turn around time
+    int wt;             //Waiting Time
 }RR;
 
-void partition(RR [], int, int);
-void merge(RR [], int, int, int);
+int sort(RR [], int);
 
 int main()
 {
-    int n, i, TCT, AvTAT = 0, AvWT = 0, Time_Quantum, count_process = 0;
+    int i, j, k, n, TCT = 0, Time_Quantum, update = 1, x = 0, rear = 0;
+    float AvTAT = 0, AvWT = 0;
 
     cout << "Enter the number of processes: ";
     cin >> n;
     cout << "Enter the time quantum for the processes: ";
     cin >> Time_Quantum;
-    RR arr[n];
+    RR arr[n];          //Array of type struct RR
+    int ReadyQue[n];    //Ready Queue
 
     for (i = 0; i < n; i++)
     {
@@ -40,64 +44,106 @@ int main()
         cin >> arr[i].burst;
         cout << "Arrival: ";
         cin >> arr[i].arrival;
-        arr[i].remaining = arr[i].burst;
+        arr[i].remaining = arr[i].burst;        //Initially the remaining time is the burst time
+        arr[i].complete = 0;                    //Completion time is 0
     }
 
-    partition (arr, 0, n - 1);
-    i = 0;
+    //Sort the processes according to the arrival time
+    sort(arr, n);
+    cout << "PROCESS\t\tARRIVAL\tBURST\n";
+    for (i = 0; i < n; i++)
+        cout << "P" << arr[i].process << "\t\t" << arr[i].arrival << "\t" << arr[i].burst << "\n";
+
+    rear = 0;
+    ReadyQue[rear] = 0;         //Process 0 is in ready queue
+
+    while (update != 0)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(TCT >= arr[j].arrival)
+            {
+                x = 0;
+                for(k = 0; k <= rear; k++)
+                    if(ReadyQue[k] == j)
+                        x++;
+                
+                //Insert the process in the ready queue if it isn't there
+                if(x == 0)
+                {
+                    rear++;
+                    ReadyQue[rear] = j;
+                }
+            }
+        }
+        if(rear == 0)
+            i = 0;
+        if(arr[i].remaining == 0)
+            i++;
+        if(i > rear)
+            i = (i - 1) % rear;         //Wrap i, so that it is within the range of rear
+        if(i <= rear)
+        {
+            if(arr[i].remaining > 0)
+            {
+                //if the remaining time of the current process is less than the given time quantum
+                //then the process will complete
+                if(arr[i].remaining < Time_Quantum)
+                {
+                    TCT = TCT + arr[i].remaining;
+                    arr[i].remaining = 0;
+                }
+                //else calculate the resulting remaining time and total completion time
+                else
+                {
+                    TCT = TCT + Time_Quantum;
+                    arr[i].remaining = arr[i].remaining - Time_Quantum;
+                }
+                arr[i].complete = TCT;          //Set the completion time of that process
+            }
+            i++;
+        }
+        update = 0;
+        
+        //Are all the process done?
+        for(j = 0; j < n; j++)
+            if(arr[j].remaining > 0)
+                update++;
+    }
+
+    //Calculate turn around time and waiting time
+    for (i = 0; i < n; i++)
+    {
+        arr[i].tat = arr[i].complete - arr[i].arrival;
+        arr[i].wt = arr[i].tat - arr[i].burst;
+    }
+
+    cout << "PROCESS\t\tTAT\tWT\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "P" << arr[i].process << "\t\t" << arr[i].tat << "\t" << arr[i].wt << "\n";
+        AvTAT = arr[i].tat + AvTAT;
+        AvWT = arr[i].wt + AvWT;
+    }
+
+    cout << "Average TAT: " << AvTAT / n << "\nAverage WT: " << AvWT / n << "\n";
     
-    //TODO here
-    while (count_process < n)
-    {
-        TCT = arr[i].remaining = arr[i].burst - Time_Quantum;
-    }
+    return 0;
 }
 
-void partition(RR arr[], int low, int high)
+int sort(RR arr[], int n)
 {
-    int mid;
+    int i, j;
+    RR temp;
 
-    if (low < high)
-    {
-        mid = (low + high) / 2;
-        partition(arr, low, mid);
-        partition(arr, mid+1, high);
-        merge(arr, low, mid, high);
-    }
-}
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++)
+            if (arr[i].arrival > arr[j].arrival)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
 
-void merge(RR arr[], int low, int mid, int high)
-{
-    int i, m, k, l;
-    RR temp[n];
-
-    i = l = low;
-    m = mid + 1;
-
-    while (l <= mid && m <= high)
-    {
-        if (arr[l].arrival <= arr[m].arrival)
-            temp[i] = arr[m];
-        else
-            temp[i] = arr[m];
-        m++;
-        i++;
-    }
-
-    if (l > mid)
-        for (k = m; k <= high; k++)
-        {
-            temp[i] = arr[k];
-            i++;
-        }
-
-    else
-        for (k = l; k <= mid; k++)
-        {
-            temp[i] = arr[k];
-            i++;
-        }
-
-    for (k = low; k <= high; k++)
-        arr[k] = temp[k];
+    return 0;
 }
